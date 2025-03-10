d4d1581eae1809cde568dd77468aeb854607284c
Nasser <mahmoudnasserelshamey@gamail.com>
2025-03-10 22:17:18 +0200

    Connect The Server and database

---
+++ BackEnd/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import dotenv from "dotenv";
+import mongoose from 'mongoose';
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+const port = process.env.PORT || 3200;
+const URL = process.env.MONGO_URL;
+
+const connectToMongoDB = async () => {
+    try {
+        await mongoose.connect(URL);
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+};
+
+connectToMongoDB();
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
